4032f68e0ace83034a970d12b43c1d0f74049dfb
Maribel-88 <139706325+Maribel-88@users.noreply.github.com>
2023-08-29 16:49:44 +0100

    Add files via upload

---
+++ MethodClassAssignSubmission/MethodClassAssignSubmission/MathOperations.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+
+namespace MethodClassAssignSubmission
+{
+     public class MathOperations
+    {
+        //Create a method that takes two integers as parameters
+        public int MathOperation (int i)
+        {
+            int result = i + 10;
+           return result;
+           
+
+
+        }
+    }
+}
---
+++ MethodClassAssignSubmission/MethodClassAssignSubmission/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+
+namespace MethodClassAssignSubmission
+{
+     class Program
+    {
+        static void Main(string[] args)
+        {
+            //Instatiate MathOperations class
+            MathOperations mathOp1 = new MathOperations();
+            //Ask the user to provide first parameter
+            Console.WriteLine("Please enter a number");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+            int results = mathOp1.MathOperation(num1);
+            Console.WriteLine(num1 + " x " + " 10 " + " = "+ results);
+            //Ask the user to enter a second number
+            Console.WriteLine("Please enter a second number");
+            int num2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("You have entered " + num2);
+
+            
+            Console.ReadLine();
+            
+        }
+    }
+}
