d6393bed50ca7679f3af765233b564b3f835e1af
Maribel-88 <139706325+Maribel-88@users.noreply.github.com>
2023-09-06 02:40:26 +0100

    Add files via upload
    Resubmission for "Method Class Assignment

---
+++ VoidFunction/VoidFunction/MathOperations1.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace VoidFunction
+{
+   class MathOperations1
+    {
+        public void MathOperation(int x, int y)
+        {
+            int numTotal = x + y;
+            Console.WriteLine("sum " + numTotal);
+        }
+    }
+}
---
+++ VoidFunction/VoidFunction/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VoidFunction
+{
+   class Program
+    {
+        static void Main(string[] args)
+        {
+            //Instatiating the class
+            MathOperations1 Math101 = new MathOperations1();
+            Console.WriteLine("Please enter an integer");
+            int x = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter a second integer");
+            int y = Convert.ToInt32(Console.ReadLine());
+            Math101.MathOperation(x, y);
+            Console.ReadKey();
+
+        }
+        
+
+        
+    }
+}
