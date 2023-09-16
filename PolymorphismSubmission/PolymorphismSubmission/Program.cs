94a858311eb0f619bfd1acfc0182dd74354834e7
Maribel-88 <139706325+Maribel-88@users.noreply.github.com>
2023-09-16 20:20:39 +0100

    Add files via upload
    final Polymorphism Assignment Submission

---
+++ PolymorphismSubmission/PolymorphismSubmission/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PolymorphismSubmission
+{
+     class Program
+    {
+        static void Main(string[] args)
+        {
+            IQuittable Resigned = new Employee() { firstName = "Maribel", lastName = "Pajares", Id = 1234 };
+            Resigned.Quit();
+            Console.ReadLine();
+        }
+    }
+}
