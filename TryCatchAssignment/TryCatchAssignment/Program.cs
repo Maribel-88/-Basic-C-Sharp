64e9c0369190ca227642b1d12632c90e8111c0d7
Maribel-88 <139706325+Maribel-88@users.noreply.github.com>
2023-11-04 15:36:32 +0000

    Add files via upload

---
+++ TryCatchAssignment/TryCatchAssignment/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TryCatchAssignment
+{
+  class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            bool validYear = false;
+            int BirthYear = 0;
+            while(!validYear) 
+            {
+                try
+                {
+                    Console.WriteLine("Enter your year of birth.");
+                    validYear = int.TryParse(Console.ReadLine(), out BirthYear);
+                    if (!validYear) Console.WriteLine("Please enter digits only, no decimals.");
+
+                    Console.ReadLine();
+                }
+                catch (Exception) 
+                {
+                    Console.WriteLine("Please enter a valid year.");
+                    Console.ReadLine() ;
+                    return;
+                }
+
+            }
+        }
+    }
+}
