caa6b0d45d4e063feee8f28e0829e12e401435bf
Maribel-88 <139706325+Maribel-88@users.noreply.github.com>
2023-12-06 03:42:22 +0000

    Add files via upload
    Final Assignment Submission Code First

---
+++ EF6Console/EF6Console/Program.cs
@@ -0,0 +1,18 @@
+﻿
+namespace EF6Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            using (var ctx = new SchoolContext())
+            {
+                var stud = new Student() { StudentName = "Bill", StudentEmail = "bill@gmail.com", Section = "Freshman"};
+                
+                ctx.Students.Add(stud);
+                ctx.SaveChanges();
+            }
+        }
+    }
+}
---
+++ EF6Console/EF6Console/Student.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EF6Console
+{
+    public class Student
+    {
+        public int StudentId { get; set; }
+        public string StudentName { get; set; }
+
+        public string StudentEmail { get; set;}
+
+        public string Section { get; set; }
+    }
+}
